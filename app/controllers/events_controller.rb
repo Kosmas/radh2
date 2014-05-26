8e624bc695914ee6f647f36106da338a281abf72
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-05-26 18:45:55 +0200

    add user authentication layout

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
   before_action :event_owner!, only: [:edit, :update, :destroy]
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   respond_to :html, :json
 
   # GET /events
